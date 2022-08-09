0cd8f6a79a4ccb1655f839bafa3f7c27bc1604c5
‘chen.home’ <1147347984@qq.com>
2022-08-09 20:41:54 +0800

    feat(login): 增加login的轮播图

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,31 @@
 <template>
-  <div text-center flex-col-center wh-full>
-    <h1 font="bold" text-5xl>{{ msg }}</h1>
-    <div m-10>
-      Already configured:
-      vue3、vite3、unocss、eslint、prettier、ts、tsx、conventional、husk、lint-staged、vue-router、axios
-    </div>
-    <div c-lightBlue><router-link to="/">Go to layout</router-link></div>
+  <div text-center flex-center wh-full>
+    <n-carousel autoplay w-500px>
+      <img
+        h-screen
+        object-cover
+        src="https://images.unsplash.com/photo-1659991689791-db84493f8544?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+      />
+      <img
+        object-cover
+        h-screen
+        src="https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+      <img
+        h-screen
+        object-cover
+        src="https://images.unsplash.com/photo-1657299156568-c94580e20fb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+      <img
+        object-cover
+        h-screen
+        src="https://images.unsplash.com/photo-1659983391845-47ea9d99e7bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+    </n-carousel>
+    <div c-lightBlue flex-1><router-link to="/">Go to layout</router-link></div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-const msg = ref('It is just a blank template.');
-</script>
+<script setup lang="ts"></script>
 
-<style lang="less" scoped></style>
+<style scoped></style>
