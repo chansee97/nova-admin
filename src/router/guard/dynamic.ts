94b22c94532d628bb83cd374111976fbdcff8165
‘chen.home’ <1147347984@qq.com>
2022-08-16 23:30:11 +0800

    feat(projects): 修改路由为根据角色动态添加

--- src/router/guard/dynamic.ts
+++ src/router/guard/dynamic.ts
@@ -1,69 +1,77 @@
 import { RouteRecordRaw } from 'vue-router';
 import { router } from '@/router';
 import { BasicLayout } from '@/layouts/index';
+import { useAuthStore } from '@/store';
 
 export async function setDynamicRoutes() {
-  const vueRoutes: RouteRecordRaw[] = [
+  const authStore = useAuthStore();
+  const vueRootRoute: RouteRecordRaw = {
+    path: '/test',
+    name: 'test',
+    redirect: '/test/test1',
+    component: BasicLayout,
+    children: [],
+  };
+  const dynamicRoutes = [
     {
-      path: '/test',
-      name: 'test',
-      redirect: '/test/test1',
-      component: BasicLayout,
-      children: [
-        {
-          path: 'test1',
-          name: 'test1',
-          component: () => import(`@/views/test/test1.vue`),
-          meta: {
-            title: '测试1',
-            icon: 'icon-park-outline:game-three',
-            requiresAuth: true,
-          },
-        },
-        {
-          path: 'test2',
-          name: 'test2',
-          component: () => import('@/views/test/test2.vue'),
-          meta: {
-            title: '测试2',
-            icon: 'carbon:aperture',
-            requiresAuth: true,
-          },
-        },
-        {
-          path: 'test3',
-          name: 'test3',
-          component: () => import('@/views/test/test3.vue'),
-          meta: {
-            title: '测试3',
-            icon: 'icon-park-outline:music-list',
-            requiresAuth: true,
-          },
-        },
-        {
-          path: '/dashboard/workbench',
-          name: 'workbench',
-          component: () => import('@/views/dashboard/workbench/index.vue'),
-          meta: {
-            title: '工作台',
-            icon: 'icon-park-outline:music-list',
-            requiresAuth: true,
-          },
-        },
-        {
-          path: '/dashboard/monitor',
-          name: 'monitor',
-          component: () => import('@/views/dashboard/monitor/index.vue'),
-          meta: {
-            title: '控制页',
-            icon: 'icon-park-outline:music-list',
-            requiresAuth: true,
-          },
-        },
-      ],
+      path: '/dashboard/workbench',
+      name: 'workbench',
+      component: () => import('@/views/dashboard/workbench/index.vue'),
+      meta: {
+        title: '工作台',
+        icon: 'icon-park-outline:music-list',
+        requiresAuth: true,
+        role: ['admin'],
+      },
+    },
+    {
+      path: '/dashboard/monitor',
+      name: 'monitor',
+      component: () => import('@/views/dashboard/monitor/index.vue'),
+      meta: {
+        title: '控制页',
+        icon: 'icon-park-outline:music-list',
+        requiresAuth: true,
+        role: ['admin'],
+      },
+    },
+    {
+      path: '/test/test1',
+      name: 'test1',
+      component: () => import(`@/views/test/test1.vue`),
+      meta: {
+        title: '测试1',
+        icon: 'icon-park-outline:game-three',
+        requiresAuth: true,
+        role: ['admin'],
+      },
+    },
+    {
+      path: '/test/test2',
+      name: 'test2',
+      component: () => import('@/views/test/test2.vue'),
+      meta: {
+        title: '测试2',
+        icon: 'carbon:aperture',
+        requiresAuth: true,
+        role: ['admin'],
+      },
+    },
+    {
+      path: '/test/test3',
+      name: 'test3',
+      component: () => import('@/views/test/test3.vue'),
+      meta: {
+        title: '测试3',
+        icon: 'icon-park-outline:music-list',
+        requiresAuth: true,
+        role: ['admin'],
+      },
     },
   ];
-  vueRoutes.forEach((route) => {
-    router.addRoute(route);
+  // 根据角色过滤后的插入根路由中
+  vueRootRoute.children = dynamicRoutes.filter((route) => {
+    return route.meta.role.includes(authStore.userInfo.role);
   });
+  router.addRoute(vueRootRoute);
 }
