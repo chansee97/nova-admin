3c39cb08c69e5587b514638d9f8e5db4871f2b6a
Coffee-crocodile <1147347984@qq.com>
2022-09-29 13:12:09 +0800

    feat(project): 丰富工作台内容

--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -37,45 +37,142 @@
         </n-space>
       </n-card>
     </n-gi>
-    <n-gi :span="15">
-      <n-card title="动态">
-        <template #header-extra><n-button type="primary" quaternary>更多动态</n-button></template>
-        <n-list>
-          <template #header>hhh</template>
-          <template #footer>fff</template>
-          <n-list-item>
-            <template #prefix>
-              <n-button>Prefix</n-button>
-            </template>
-            <template #suffix>
-              <n-button>Suffix</n-button>
-            </template>
-            <n-thing title="Thing" title-extra="extra" description="description">
-              Biu
-              <br />
-              Biu
-              <br />
-              Biu
-              <br />
-            </n-thing>
-          </n-list-item>
-          <n-list-item>
-            <n-thing title="Thing" title-extra="extra" description="description" />
-          </n-list-item>
-        </n-list>
-      </n-card>
+    <n-gi :span="17">
+      <n-space vertical :size="16">
+        <n-card title="项目">
+          <template #header-extra><n-button type="primary" quaternary>更多</n-button></template>
+          <n-grid :x-gap="8" :y-gap="8">
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+            <n-gi :span="8">
+              <n-card title="卡片" hoverable>
+                卡片内容
+                <template #action>#action</template>
+              </n-card>
+            </n-gi>
+          </n-grid>
+        </n-card>
+        <n-card title="动态">
+          <template #header-extra><n-button type="primary" quaternary>更多</n-button></template>
+          <n-list hoverable>
+            <n-list-item>
+              <template #prefix>
+                <n-avatar round :size="48" :src="authStore.userInfo?.avatar" />
+              </template>
+              <n-thing
+                title="客怎车"
+                title-extra="09/29/2022"
+                description="是“我的客厅怎么会有车”的缩写，指那些在车道间肆意穿梭，把马路当客厅的人，多有嘲讽意味，主要用于车祸视频中。"
+              />
+            </n-list-item>
+            <n-list-item>
+              <template #prefix>
+                <n-avatar round :size="48" :src="authStore.userInfo?.avatar" />
+              </template>
+              <n-thing
+                title="街健五大神技"
+                title-extra="09/29/2022"
+                description="街头健身五大神技，包括1.单手引体向上。2.慢速双力臂。3.人旗。4.前水平。5.俄式挺身。"
+              />
+            </n-list-item>
+            <n-list-item>
+              <template #prefix>
+                <n-avatar round :size="48" :src="authStore.userInfo?.avatar" />
+              </template>
+              <n-thing
+                title="天下岂有七十年太子乎"
+                title-extra="09/29/2022"
+                description="★含义：用来调侃由于英国女王超长的在位时间，导致其长子查理斯王子成为史上等待王位时间最久的王储 "
+              />
+            </n-list-item>
+            <n-list-item>
+              <template #prefix>
+                <n-avatar round :size="48" :src="authStore.userInfo?.avatar" />
+              </template>
+              <n-thing
+                title="你干嘛～哈哈～哎哟～"
+                title-extra="09/29/2022"
+                description="出自著名偶像练习生、练习时长两年半、背带异常梳中分的蔡徐坤在2018年的一档综艺节目偶像练习生中出现的一幕"
+              />
+            </n-list-item>
+          </n-list>
+        </n-card>
+      </n-space>
     </n-gi>
-    <n-gi :span="9">
-      <n-card title="快捷入口">
-        <n-grid :x-gap="8" :y-gap="8">
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-          <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
-        </n-grid>
-      </n-card>
+    <n-gi :span="7">
+      <n-space vertical :size="16">
+        <n-card title="公告">
+          <template #header-extra><n-button type="primary" quaternary>更多</n-button></template>
+          <n-list>
+            <n-list-item>
+              <template #prefix>
+                <n-tag :bordered="false" type="info" size="small">通知</n-tag>
+              </template>
+              <n-button text>漂洋过海上大专</n-button>
+            </n-list-item>
+            <n-list-item>
+              <template #prefix>
+                <n-tag :bordered="false" type="success" size="small">消息</n-tag>
+              </template>
+              <n-button text>你在玩很新的东西</n-button>
+            </n-list-item>
+            <n-list-item>
+              <template #prefix>
+                <n-tag :bordered="false" type="warning" size="small">活动</n-tag>
+              </template>
+              <n-button text>上岸第一剑，先斩意中人</n-button>
+            </n-list-item>
+          </n-list>
+        </n-card>
+        <n-card title="快捷入口">
+          <n-grid :x-gap="8" :y-gap="8">
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+            <n-gi :span="8"><n-card title="卡片" hoverable>卡片内容</n-card></n-gi>
+          </n-grid>
+        </n-card>
+        <n-card title="任务进度">
+          <n-timeline>
+            <n-timeline-item content="啊" />
+            <n-timeline-item type="success" title="成功" content="哪里成功" time="2018-04-03 20:46" />
+            <n-timeline-item type="error" content="哪里错误" time="2018-04-03 20:46" />
+            <n-timeline-item type="warning" title="警告" content="哪里警告" time="2018-04-03 20:46" />
+            <n-timeline-item type="info" title="信息" content="是的" time="2018-04-03 20:46" line-type="dashed" />
+            <n-timeline-item content="啊" />
+          </n-timeline>
+        </n-card>
+      </n-space>
     </n-gi>
   </n-grid>
 </template>
