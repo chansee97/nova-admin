9ff3494045ba19110691358208b8db255dcfbc89
chen.home <chen.dev@foxmail.com>
2023-07-19 22:12:22 +0800

    fix: modify env name

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 /** 不同请求服务的环境配置 */
 export const proxyConfig: Record<ServiceEnvType, ServiceEnvConfig> = {
-  development: {
+  dev: {
     url: 'https://mock.mengxuegu.com/mock/61e4df7c17249f68847fc191/api',
     urlPattern: '/url-pattern',
     secondUrl: 'http://localhost:8081',
@@ -12,7 +12,7 @@ export const proxyConfig: Record<ServiceEnvType, ServiceEnvConfig> = {
     secondUrl: 'http://localhost:8081',
     secondUrlPattern: '/second-url-pattern',
   },
-  production: {
+  prod: {
     url: 'http://localhost:8080',
     urlPattern: '/url-pattern',
     secondUrl: 'http://localhost:8081',
