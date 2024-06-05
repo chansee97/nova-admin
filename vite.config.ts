bf1cfcdd27a4186147aa8e9393d1be0333a01aa2
chansee97 <chen.dev@foxmail.com>
2024-06-05 15:54:27 +0800

    fix: `jiti` install error

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(({ mode }) => {
       reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告
     },
     optimizeDeps: {
-      include: ['echarts', 'md-editor-v3', '@tinymce/tinymce-vue'],
+      include: ['echarts', 'md-editor-v3'],
     },
   }
 })
