5cb0ca39dd0d861290a076ea9b793f45d4a0d01b
Rock Chen <chen.dev@foxmail.com>
2025-01-16 13:12:24 +0000

    feat: add draggable tab bar items and refactor tab rendering

--- src/layouts/components/index.ts
+++ src/layouts/components/index.ts
@@ -1,17 +1,17 @@
 import BackTop from './common/BackTop.vue'
 import Setting from './common/Setting.vue'
-
 import SettingDrawer from './common/SettingDrawer.vue'
+
 import Breadcrumb from './header/Breadcrumb.vue'
 import CollapaseButton from './header/CollapaseButton.vue'
 import FullScreen from './header/FullScreen.vue'
 import Notices from './header/Notices.vue'
 import Search from './header/Search.vue'
-
 import UserCenter from './header/UserCenter.vue'
 
 import Logo from './sider/Logo.vue'
 import Menu from './sider/Menu.vue'
+
 import TabBar from './tab/TabBar.vue'
 
 export {
