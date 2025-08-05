a1085e1922f88d2900d3fe2fe900a41c527e1e57
chansee97 <chen.dev@foxmail.com>
2025-08-05 16:44:49 +0800

    fix: full-content padding error

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -6,11 +6,15 @@ const routeStore = useRouteStore()
 </script>
 
 <template>
-  <n-el>
+  <n-el
+    class="h-full"
+    :class="[
+      appStore.layoutMode === 'full-content' ? 'p-0' : 'p-16px',
+    ]"
+    style="background-color: var(--action-color);"
+  >
     <router-view
       v-slot="{ Component, route }"
-      class="flex-1 p-16px"
-      style="background-color: var(--action-color);"
     >
       <transition :name="appStore.transitionAnimation" mode="out-in">
         <keep-alive :include="routeStore.cacheRoutes">
